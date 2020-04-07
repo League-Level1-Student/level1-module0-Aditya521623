c71682378034d521e1e548b85c4b2263ffc2f9f6
Aditya521623 <54295036+Aditya521623@users.noreply.github.com>
2020-04-06 19:56:47 -0700

    worked on module 0

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -20,33 +20,46 @@ public class PhotoQuiz {
 
 		JFrame quizWindow = new JFrame();
 		quizWindow.setVisible(true);
+		
+		quizWindow.setVisible(true);
 		// This will make sure the program exits when you close the window
-		quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
 
+		String imageUrl="http://free-hd-wall-papers.com/images/puppy-images/puppy-images-1.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component image=loadImage(imageUrl);
 		// 3. use the "createImage()" method below to initialize your Component
-
 		// 4. add the image to the quiz window
-
+quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String ans =JOptionPane.showInputDialog("Is this dog's fur white? This is a yes or no question. ");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(ans.equalsIgnoreCase("yes")) {
+	JOptionPane.showMessageDialog(null,"Correct");
+}
+else
+{
+	JOptionPane.showMessageDialog(null, "Incorrect");
+}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
+//quizWindow.remove(image);
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
+//String imageUrl2="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pcmag.com%2Freviews%2Fapple-ipad-2019&psig=AOvVaw2P2kVQGQis1wmZ_TpYk9PV&ust=1586313350009000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMilz_Gj1egCFQAAAAAdAAAAABAS";
 
+//Component image2 = loadImage("ipad.png"); 
+
+
+//loadImage(imageUrl2);
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
@@ -57,7 +70,7 @@ public class PhotoQuiz {
 
 	}
 
-	private Component createImage(String imageUrl) throws MalformedURLException {
+	private Component loadImage(String imageUrl) throws MalformedURLException {
 		URL url = new URL(imageUrl);
 		Icon icon = new ImageIcon(url);
 		JLabel imageLabel = new JLabel(icon);
